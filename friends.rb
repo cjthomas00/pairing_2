ad4012835c174dc136ff13e0492e675d19be86dd
Caleb Thomas <cjthomas00@comcast.net>
2022-10-18 19:31:56 -0600

    Add friends if statement

---
+++ friends.rb
@@ -0,0 +1,10 @@
+best_friend = "Joe"
+other_friend = "Emily"
+
+if best_friend == "Sam"
+    p "Hi Sam, how are you today?"
+elsif other_friend == "Emily"
+    p "Hi Emily, What's up?"
+else 
+    p "Hi, how are you today?"
+end
